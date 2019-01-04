9ac61624528b206c5d9383b8f7a22bf637d238b5
yuangezhizao <root@yuangezhizao.cn>
2019-01-05 01:56:47 +0800

    Init

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+
+python task_daily.py ${access_key}
+
+git config user.name "yuangezhizao"
+git config user.email "root@yuangezhizao.cn"
+
+git add .
+git commit -m "Travis CI Cron Jobs at `date +"%Y-%m-%d %H:%M:%S"`"
+git push --quiet "https://${TravisCIToken}@https://github.com/yuangezhizao/bilibili_splash_fetch.git" master:master
---
+++ task_daily.py
@@ -0,0 +1,33 @@
+import datetime
+import hashlib
+import json
+import sys
+import time
+
+import requests
+
+
+def calc_sign(param):
+    sign_hash = hashlib.md5()
+    sign_hash.update(f"{param}560c52ccd288fed045859ed18bffd973".encode('utf-8'))
+    return sign_hash.hexdigest()
+
+
+def bilibili_splash_fetch(access_key):
+    url = 'https://app.bilibili.com/x/v2/splash/list'
+    temp_params = f'access_key={access_key}&appkey=1d8b6e7d45233436&birth=0218&build=5360000&height=2560' \
+                      f'&mobi_app=android&platform=android&ts=' + str(int((time.time() * 1000))) + '&width=1536'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 BiliDroid/5.36.0 (bbcallen@gmail.com)'
+    }
+    sign = calc_sign(temp_params)
+    params = temp_params + '&sign=' + sign
+    r = requests.get(url, headers=headers, params=params)
+    r.json()['cache_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    json_file_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.json'
+    with open('./json_data/' + json_file_name, 'w', encoding='utf-8') as f:
+        json.dump(r.json(), f)
+
+
+if __name__ == "__main__":
+    bilibili_splash_fetch(sys.argv[1])
