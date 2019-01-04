fa271d6f69265bc1a540e46940e9c62ccda3530b
yuangezhizao <root@yuangezhizao.cn>
2019-01-05 02:57:03 +0800

    Solve "detached HEAD"

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ python task_daily.py ${access_key}
 git config user.name "yuangezhizao"
 git config user.email "root@yuangezhizao.cn"
 
+git checkout master
 git add .
 git commit -m "Travis CI Cron Jobs at `date +"%Y-%m-%d %H:%M:%S"`"
 git push --force "https://${TravisCIToken}@github.com/yuangezhizao/bilibili_splash_fetch.git" master:master
